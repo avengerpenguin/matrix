97d8ecd4d1a7e5b80b4dc3880cf26715cee882e9
Ross Fenning <ross.fenning@bridge-u.com>
2025-04-29 13:31:51 +0100

    Add hosts, ignore vars

--- run.sh
+++ run.sh
@@ -2,8 +2,19 @@
 
 set -ex -o pipefail
 
-cd "$(dirname "$0")"
+ROOT_DIR="$(dirname "$(realpath "$0")")"
+cd "$ROOT_DIR"
 
 cd infra
 tofu init
-tofu plan
+tofu apply
+
+
+cd "$ROOT_DIR"
+
+ln -sfn "$ROOT_DIR/inventory" matrix-docker-ansible-deploy/inventory
+
+cd matrix-docker-ansible-deploy
+just update
+ansible-playbook -i inventory/hosts setup.yml --tags=install-all,ensure-matrix-users-created,start
+#ansible-playbook -i inventory/hosts setup.yml --extra-vars='username=avengerpenguin password=aeYHhaGXm5BRk2MiaBD2C1rSde3WE9pzwsTfbaSDxdrEiNgGDKHsE5xWoDKZ58io admin=yes' --tags=register-user
